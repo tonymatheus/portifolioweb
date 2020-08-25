daef19bf8312dafc94a5bfcbc43d8ce43ce90d19
tonymatheus <tony.chaves14@gmail.com>
2020-08-24 22:26:04 -0300

    :package: Adiciona funcções de interação com  a tela

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ document
   .addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
   );
+
+function openRepository() {
+  window.open("https://github.com/tonymatheus/FinacasApp");
+}
