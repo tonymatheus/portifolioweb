fcc5d858d3672fd61fddb1bd91ab0818b7820ea8
tonymatheus <tony.chaves14@gmail.com>
2020-08-27 17:46:49 -0300

    :package: Adiciona Calculo de orçamento com js

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ document
 function openRepository() {
   window.open("https://github.com/tonymatheus/FinacasApp");
 }
+
+//Calculo  de página
+document.querySelector("#qtde").addEventListener("change", atualizaPreco);
+document.querySelector("#js").addEventListener("change", atualizaPreco);
+document.querySelector("#layout-sim").addEventListener("change", atualizaPreco);
+document.querySelector("#layout-nao").addEventListener("change", atualizaPreco);
+document.querySelector("#prazo").addEventListener("change", function () {
+  const prazo = document.querySelector("#prazo").value;
+
+  document.querySelector(
+    "label[for=prazo]"
+  ).innerHTML = `Prazo:${prazo} Semanas`;
+
+  atualizaPreco();
+});
+
+function atualizaPreco() {
+  const quantidade = document.querySelector("#qtde").value;
+  const temJs = document.querySelector("#js").checked;
+  const PrecisaDelayout = document.querySelector("#layout-sim").checked;
+  const prazo = document.querySelector("#prazo").value;
+
+  let preco = quantidade * 100;
+  if (temJs) preco *= 1.1;
+  if (PrecisaDelayout) preco += 500;
+  let taxaDeUrgencia = 1 - prazo * 0.1;
+  preco *= 1 + taxaDeUrgencia;
+
+  document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`;
+}
